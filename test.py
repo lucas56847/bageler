b5e0abf67ce710c9ffcc1e480d81c1df0edf9fea
lucas56847 <mhlong@clemson.edu>
2023-05-26 21:18:51 -0400

    updated to include easter eggs

--- test.py
+++ test.py
@@ -1,15 +1,37 @@
 import os
-import discord
+import discord,random
 from dotenv import load_dotenv
 load_dotenv()
 
 token = os.getenv("token")
+guildname = os.getenv('discord_guild')
+intents = discord.Intents.default()
+intents.presences = True
+intents.members = True
+intents.message_content = True
 
-client = discord.Client()
+client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has ARRIVED')
+    print(f'list: {client.guilds}')
+   # print(f'listmembers: {guild.members}')
+
+
+    for guild in client.guilds:
+        if guild.name == guildname:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+   # members = '\n - '.join([member.name for member in guild.members])
+    for members in guild.members:
+      print(f'Guild Member:\n - {members.name}')    
 
 @client.event
 async def on_message(message):
@@ -17,5 +39,38 @@ async def on_message(message):
         return
     if message.content =='hello':
         await message.channel.send("I'm eating glass.")
-
+        return
+    if message.content =='Julia':
+        await message.channel.send(file=discord.File('agony.png'))
+        return
+    am_psycho_quotes = ['Feed me a stray cat.',
+                        'I have to return some videotapes.',
+                        'Do you like Huey Lewis and The News?',
+                        'Yes it is! In \'87, Huey released this, Fore, their most accomplished album. ' +
+                        'I think their undisputed masterpiece is "Hip to be Square", a song so catchy, most people probably don\'t ' +
+                        'listen to the lyrics. But they should, because it\'s not just about the pleasures of conformity, and the ' +
+                        'importance of trends, it\'s also a personal statement about the band itself.',
+                        'Hey Paul!',
+                        'Try getting a reservation at Dorsia now, you fucking stupid bastard!',
+                        'Let\'s see Paul Allen\'s card.',
+                        'Just cool it with the anti-Semitic remarks.',
+                        'There are no more barriers to cross. All I have in common with the uncontrollable and the insane, the vicious ' +
+                        'and the evil, all the mayhem I have caused and my utter indifference toward it I have now surpassed. '+
+                        'My pain is constant and sharp, and I do not hope for a better world for anyone. In fact, I want my '+
+                        'pain to be inflicted on others. I want no one to escape. But even after admitting this, there is no catharsis; '+
+                        'my punishment continues to elude me, and I gain no deeper knowledge of myself. No new knowledge can be extracted '+
+                        'from my telling. This confession has meant nothing.',
+                        'There is a moment of sheer panic when I realize that Paul\'s apartment overlooks the park... and is obviously more '+
+                        'expensive than mine',
+                        'Impressive. Very nice.',
+                        'Why not, you stupid bastard?',
+                        'I\'m just a happy camper! Rockin\' and a-rollin\'!',
+                        'Hi, this is Paul Allen. I\'m being called away to London for a few days. Meredith, I\'ll call you when I get back. ' +
+                        'Hasta la vista, baby']
+    if message.content == 'Bateman':
+        response = random.choice(am_psycho_quotes) 
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+        return
 client.run(token)
